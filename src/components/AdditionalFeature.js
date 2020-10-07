23fc9cd25b0f6fefd6d7a9cb1a62692badec2155
brandononeal <baoneal95@gmail.com>
2020-10-07 09:03:21 -0700

    mvp nearly complete

--- src/components/AdditionalFeature.js
+++ src/components/AdditionalFeature.js
@@ -1,13 +1,21 @@
 import React from "react";
+import { addFeature } from "../actions/actions";
+import { connect } from "react-redux";
 
-const AdditionalFeature = (props) => {
+export const AdditionalFeature = (props) => {
   return (
     <li>
-      {/* Add an onClick that will let you add a feature to your car */}
-      <button className="button">Add</button>
+      <button
+        className="button"
+        onClick={() => props.addFeature(props.feature)}
+      >
+        Add
+      </button>
       {props.feature.name} (+{props.feature.price})
     </li>
   );
 };
 
-export default AdditionalFeature;
+export const mapStateToProps = () => {};
+
+export default connect(mapStateToProps, { addFeature })(AdditionalFeature);
